e7d6e6a00c682f3b81be185160f6081b91258e27
[Ritik] <ritikkumar8077008067@gmail.com>
2024-10-09 23:07:25 +0530

    completed

--- src/origin/allowedOrigin.js
+++ src/origin/allowedOrigin.js
@@ -2,6 +2,6 @@ export const allowedOrigins = [
     "http://localhost:5173",
     "https://gold-ornament-frontend-git-main-ritik-latiyans-projects.vercel.app",
     "https://gold-ornament-frontend-kqqkgm1j3-ritik-latiyans-projects.vercel.app",
-    "https://gold-ornament-frontend.vercel.app",
+    "https://bharat-gold-test.vercel.app",
     
 ] ;
