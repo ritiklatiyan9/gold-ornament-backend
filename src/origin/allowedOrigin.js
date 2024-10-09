1e21b8e61bb9c5b8080687aea72ad9e0068098fa
[Ritik] <ritikkumar8077008067@gmail.com>
2024-10-09 22:56:08 +0530

    added origins

--- src/origin/allowedOrigin.js
+++ src/origin/allowedOrigin.js
@@ -1,9 +1,7 @@
 export const allowedOrigins = [
     "http://localhost:5173",
-    "https://bytematrix-git-main-ritik-latiyans-projects.vercel.app",
-    "https://bytematrix-caprlwi99-ritik-latiyans-projects.vercel.app",
-    "https://bytematrixsolutions.com",
-    "https://bmc-dashboard-git-main-ritik-latiyans-projects.vercel.app",
-    "https://bmc-dashboard.vercel.app",
-    "https://bmc-dashboard-puqmdy0by-ritik-latiyans-projects.vercel.app"
+    "https://gold-ornament-frontend-git-main-ritik-latiyans-projects.vercel.app",
+    "https://gold-ornament-frontend-kqqkgm1j3-ritik-latiyans-projects.vercel.app",
+    "https://gold-ornament-frontend.vercel.app",
+    
 ] ;
